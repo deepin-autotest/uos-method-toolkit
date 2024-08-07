3bd435211d2aef20fd87560bc18f395224f455d7
mikigo <huangmingqiang@uniontech.com>
2024-08-07 17:29:52 +0800

    update

--- scripts/gen_md.py
+++ scripts/gen_md.py
@@ -1,9 +1,8 @@
 import os
-import pathlib
 
-rootdir = pathlib.Path(__file__).parent.parent
-umtk_path = rootdir / 'umtk'
-docs_path = rootdir / 'docs'
+rootdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+umtk_path = os.path.join(rootdir, 'umtk')
+docs_path = os.path.join(rootdir, 'docs')
 
 for root, dirs, files in os.walk(umtk_path):
     for file in files:
@@ -17,7 +16,7 @@ for root, dirs, files in os.walk(umtk_path):
             md_relpath = md_path.split("umtk")[-1].strip("/")
             dd = len(md_relpath.split('/')) + 1
             docs_md_path = os.path.join(docs_path, "umtk", md_relpath)
-            os.makedirs(pathlib.Path(docs_md_path).parent, exist_ok=True)
+            os.makedirs(os.path.dirname(docs_md_path), exist_ok=True)
             with open(docs_md_path, "w") as md_file:
                 md_file.write(f'<!--@include: {"../" * dd}umtk/{md_relpath}-->')
         if file.endswith('.py'):
@@ -25,7 +24,7 @@ for root, dirs, files in os.walk(umtk_path):
             py_relpath = py_path.split("umtk")[-1].strip("/")
             dd = len(py_relpath.split('/')) + 1
             docs_py_path = os.path.join(docs_path, "umtk", py_relpath).replace(".py", ".md")
-            os.makedirs(pathlib.Path(docs_py_path).parent, exist_ok=True)
+            os.makedirs(os.path.dirname(docs_py_path), exist_ok=True)
             with open(docs_py_path, "w") as py_file:
                 py_file.write('```python\n')
                 py_file.write(f'<!--@include: {"../" * dd}umtk/{py_relpath}-->\n')
